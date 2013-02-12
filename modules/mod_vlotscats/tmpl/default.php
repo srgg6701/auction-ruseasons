8c63f261dffb0e7dac9bcbf9d39844194fa23d28
serge-serg <srgg67@gmail.com>
2013-02-12 22:00:52 +0400

    Окончательный переход на новый способ извлечения данных для панели категорий предметов.

--- modules/mod_vlotscats/tmpl/default.php
+++ modules/mod_vlotscats/tmpl/default.php
@@ -2,50 +2,28 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 // get categories:
-$lots=modVlotscatsHelper::getTopCatCounts();
+$lots=modVlotscatsHelper::getTopCatCounts();?>
+<br/>
+<?
 foreach($lots as $top_cat_id => $array){
-	ob_start();?>
-<ul>
-<?	$top_cat_count=0;
+	$top_cat_count=0;
+	$sub_cats='
+<ul>';
 	foreach($array as $key=>$array_data):
 		if ($key=='children'):
-			foreach($array_data as $i=>$category_data):?>
-	<li><a href="<?=JRoute::_('index.php?option=com_')?>"><?=$category_data['category_name']?></a> (<? 
-			$product_count=(int)$category_data['product_count'];
-			echo $product_count;
-			$top_cat_count+=$product_count;
-		?>)</li>
-	<?		//var_dump($category_data);
+			foreach($array_data as $i=>$category_data):
+				$product_count=(int)$category_data['product_count'];
+				$top_cat_count+=$product_count;
+				$sub_cats.='
+	<li><a href="'.JRoute::_('index.php?option=com_').'">'.$category_data['category_name'].'</a> ('. $product_count .')</li>';
 			endforeach;
 		endif;
-	endforeach;?>
-</ul>
-<? 	$sub_cats=ob_get_contents();
-	ob_clean();?>
+	endforeach;
+$sub_cats.='
+</ul>';?>
 <h3>
 	<a href="<?=JRoute::_('index.php?option=com_')?>"><?=$array['top_category_name']?></a>
 	<span class="lots_count">(<?=$top_cat_count?>)</span>
 </h3>
 <?	echo $sub_cats;	
-}
-
-die();
-
-$VirtuemartViewCategory=modVlotscatsHelper::getVMitems();
-echo "<br/>";
-
-foreach($VirtuemartViewCategory->categories as $i=>$cat){
-	$header=$VirtuemartViewCategory->escape($cat->category_name);
-	if(!$item_count=$VirtuemartViewCategory->catmodel->countProducts($cat->virtuemart_category_id))
-		$item_count = '0';
-	if($cat->category_parent_id == '0'):?>
-<h3>
-	<a href="<?=JRoute::_('index.php?option=com_'.$link)?>"><?=$header?></a>
-	<span class="lots_count">(<?=$item_count?>)</span>
-</h3>
-<?	else:?>
-<ul>
-	<li><a href="<?=JRoute::_('index.php?option=com_'.$link)?>"><?=$header?></a> (<?=$item_count?>)</li>
-</ul>
-<?	endif;
-}die();?>
+}?>
