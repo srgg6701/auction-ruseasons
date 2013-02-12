2284a401e11e62b0d06c51ecea5ad332c66ecf53
serge-serg <srgg67@gmail.com>
2013-02-12 21:46:08 +0400

    спасатели на помощь!

--- modules/mod_vlotscats/helper.php
+++ modules/mod_vlotscats/helper.php
@@ -52,9 +52,78 @@ class modVlotscatsHelper extends JModuleHelper
 				} else ...
 		*/
 		$categories = $model->getCategoryTree(0, 0, false, $VirtuemartViewCategory->lists['search'], true);
-		//var_dump($categories);
+		// var_dump($categories);die();
 		$VirtuemartViewCategory->assignRef('categories', $categories);
 		$VirtuemartViewCategory->assignRef('catmodel',	$model);
 		return 	$VirtuemartViewCategory;
+	}
+/**
+ * Описание
+ * @package
+ * @subpackage
+ */
+	function getTopCategories($db=false){
+		$query='SELECT cats.virtuemart_category_id, 
+        cats_ru.category_name,
+		cats_ru.slug AS "alias"
+FROM #__virtuemart_categories AS cats
+LEFT JOIN #__virtuemart_categories_ru_ru AS cats_ru 
+    ON  cats_ru.virtuemart_category_id = cats.virtuemart_category_id
+LEFT JOIN #__virtuemart_category_categories AS cat_cats 
+    ON  cat_cats.id = cats.virtuemart_category_id
+WHERE cat_cats.category_parent_id = 0 AND cats.`published` = "1"
+ORDER BY cats.ordering'; 
+		if(!$db) 
+			$db=JFactory::getDBO();
+		$db->setQuery($query);
+		return $db->loadAssocList(); 
+	}
+/**
+ * Описание
+ * @package
+ * @subpackage
+ */
+	function getTopCatCounts($db=false){
+		$db=JFactory::getDBO();
+		$top_cats=modVlotscatsHelper::getTopCategories($db);
+		/* 0 => 
+			array
+			  'virtuemart_category_id' => string '21' (length=2)
+			  'category_name' => string 'Онлайн торги' (length=23)
+		  1 => 
+			array
+			  'virtuemart_category_id' => string '22' (length=2)
+			  'category_name' => string 'Очные торги' (length=21)
+		  2 => 
+			array
+			  'virtuemart_category_id' => string '23' (length=2)
+			  'category_name' => string 'Магазин' (length=14)
+	  	*/
+		foreach($top_cats as $i=>$top_cat){
+			$query='SELECT cats.virtuemart_category_id, 
+        cats_ru.category_name,
+        cats_ru.slug AS "alias",
+        (   SELECT count(p.virtuemart_product_id)
+			FROM `auc13_virtuemart_products` AS p, `auc13_virtuemart_product_categories` AS pc
+			WHERE pc.`virtuemart_category_id` = cats.virtuemart_category_id
+			AND p.`virtuemart_product_id` = pc.`virtuemart_product_id`
+			AND p.`published` = "1"
+        ) AS "product_count"
+FROM auc13_virtuemart_categories AS cats
+LEFT JOIN auc13_virtuemart_categories_ru_ru AS cats_ru 
+    ON  cats_ru.virtuemart_category_id = cats.virtuemart_category_id
+ LEFT JOIN auc13_virtuemart_category_categories AS cat_cats 
+    ON  cat_cats.id = cats.virtuemart_category_id
+WHERE cat_cats.category_parent_id = '.$top_cat['virtuemart_category_id'].' 
+	AND cats.`published` = "1"
+ORDER BY cat_cats.category_parent_id,cats.ordering';
+			$db->setQuery($query);
+			$records[$top_cat['virtuemart_category_id']]=array(
+						'top_category_alias'=>$top_cat['alias'],
+						'top_category_name'=>$top_cat['category_name'],
+						'children'=>$db->loadAssocList()
+				); 
+		}
+		return $records;
 	}
 }
--- modules/mod_vlotscats/tmpl/default.php
+++ modules/mod_vlotscats/tmpl/default.php
@@ -2,8 +2,38 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 // get categories:
+$lots=modVlotscatsHelper::getTopCatCounts();
+foreach($lots as $top_cat_id => $array){
+	ob_start();?>
+<ul>
+<?	$top_cat_count=0;
+	foreach($array as $key=>$array_data):
+		if ($key=='children'):
+			foreach($array_data as $i=>$category_data):?>
+	<li><a href="<?=JRoute::_('index.php?option=com_')?>"><?=$category_data['category_name']?></a> (<? 
+			$product_count=(int)$category_data['product_count'];
+			echo $product_count;
+			$top_cat_count+=$product_count;
+		?>)</li>
+	<?		//var_dump($category_data);
+			endforeach;
+		endif;
+	endforeach;?>
+</ul>
+<? 	$sub_cats=ob_get_contents();
+	ob_clean();?>
+<h3>
+	<a href="<?=JRoute::_('index.php?option=com_')?>"><?=$array['top_category_name']?></a>
+	<span class="lots_count">(<?=$top_cat_count?>)</span>
+</h3>
+<?	echo $sub_cats;	
+}
+
+die();
+
 $VirtuemartViewCategory=modVlotscatsHelper::getVMitems();
 echo "<br/>";
+
 foreach($VirtuemartViewCategory->categories as $i=>$cat){
 	$header=$VirtuemartViewCategory->escape($cat->category_name);
 	if(!$item_count=$VirtuemartViewCategory->catmodel->countProducts($cat->virtuemart_category_id))
@@ -18,4 +48,4 @@ foreach($VirtuemartViewCategory->categories as $i=>$cat){
 	<li><a href="<?=JRoute::_('index.php?option=com_'.$link)?>"><?=$header?></a> (<?=$item_count?>)</li>
 </ul>
 <?	endif;
-}?>
+}die();?>
