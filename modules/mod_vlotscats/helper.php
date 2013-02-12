334ab09ea3c2179e365a296fc7815384fb931da6
serge-serg <srgg67@gmail.com>
2013-02-12 15:10:51 +0400

    Завершение интеграции скриптов магазина в панель лотов в mod_vlotscats.

--- modules/mod_vlotscats/helper.php
+++ modules/mod_vlotscats/helper.php
@@ -1,18 +1,16 @@
-﻿<?php
+<?php
 // No direct access
 defined('_JEXEC') or die('Restricted access'); 
+$virtuemart_path=JPATH_ADMINISTRATOR.DS.'components'.DS.'com_virtuemart'.DS;
+require_once $virtuemart_path.'helpers'.DS.'vmmodel.php';
+require_once $virtuemart_path.'models'.DS.'category.php';
+require_once $virtuemart_path.'views'.DS.'category'.DS.'view.html.php';
 /**
  * получим методы основного класса:
  */
 class modVlotscatsHelper extends JModuleHelper
 {	
-	public function getVMitems(){
-		$virtuemart_path=JPATH_ADMINISTRATOR.DS.'components'.DS.'com_virtuemart'.DS;
-		require_once $virtuemart_path.'helpers'.DS.'vmmodel.php';
-		
-		require_once $virtuemart_path.'models'.DS.'category.php';
-		
-		require_once $virtuemart_path.'views'.DS.'category'.DS.'view.html.php';
+	public static function getVMitems(){
 /*	count: 0
 	===========================================================
 	  public 'virtuemart_category_id' => string '23' (length=2)
@@ -35,18 +33,25 @@ class modVlotscatsHelper extends JModuleHelper
 		  public 'category_child_id' => string '14' (length=2)
 		  public 'category_parent_id' => string '23' (length=2)
 		  public 'shared' => string '0' (length=1)
-		  public 'level' => int 2
-*/
-		
+		  public 'level' => int 2	*/		
 		$VirtuemartViewCategory=new VirtuemartViewCategory;
 		$model = VmModel::getModel('Category','VirtueMartModel');		
 		$VirtuemartViewCategory->SetViewTitle('CATEGORY_S');
 		$VirtuemartViewCategory->assignRef('catmodel',	$model);
 		$VirtuemartViewCategory->addStandardDefaultViewCommands();
-		$VirtuemartViewCategory->addStandardDefaultViewLists($model,'category_name');
-		
-		// ВНИМАНИЕ! Последний аргумент отсутствует в нативном методе getCategoryTree() и добален здесь для того, чтобы снять лимит вывода данных. В случае переустановки VirtueMart - добавить заново как ",$unlimited = false"
-		$categories = $model->getCategoryTree(0,0,false,$VirtuemartViewCategory->lists['search'],true);
+		$VirtuemartViewCategory->addStandardDefaultViewLists($model,'category_name');		
+		/* ВНИМАНИЕ! Последний аргумент (true) отсутствует в нативном методе VirtueMartModelCategory::getCategoryTree() и добавлен здесь для того, чтобы снять лимит вывода данных. В случае переустановки VirtueMart - добавить заново как ",$unlimited = false"!
+			А также заменить фрагмент кода:
+
+				if(empty($limit)){
+				return $sortedCats;
+				} else ...
+				
+			на:	if(empty($limit)||$unlimited){
+				return $sortedCats;
+				} else ...
+		*/
+		$categories = $model->getCategoryTree(0, 0, false, $VirtuemartViewCategory->lists['search'], true);
 		//var_dump($categories);
 		$VirtuemartViewCategory->assignRef('categories', $categories);
 		$VirtuemartViewCategory->assignRef('catmodel',	$model);
