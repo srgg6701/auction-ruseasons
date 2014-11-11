f2cdd920e81089a1442f8769ca58f71ec5bf7051
srgg6701 <srgg67@gmail.com>
2014-11-11 22:15:32 +0300

    Закомментирована форма повторного (расширенного) поиска.

--- components/com_search/views/search/tmpl/default_form.php
+++ components/com_search/views/search/tmpl/default_form.php
@@ -10,8 +10,7 @@
 defined('_JEXEC') or die;
 $lang = JFactory::getLanguage();
 $upper_limit = $lang->getUpperLimitSearchWord();
-?>
-
+/*?>
 <form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_search');?>" method="post">
 
 	<fieldset class="word">
@@ -72,3 +71,4 @@ $upper_limit = $lang->getUpperLimitSearchWord();
 <?php endif; ?>
 
 </form>
+<?php   */
